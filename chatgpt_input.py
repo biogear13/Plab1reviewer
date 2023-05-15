6832ef0a5604ad37e6d048b58f4a7d31d98c084a
biogear13 <generoso.gr.roberto@gmail.com>
2023-05-15 14:32:10 +0100

    Created a rudimentary quiz program

--- chatgpt_input.py
+++ chatgpt_input.py
@@ -5,40 +5,50 @@ working_df = pd.read_csv('plab_chatgpt.csv')
 # random question generator
 import random
 def random_question(df):
+    answered = pd.read_csv('answered.csv')
+    if answered['answered'].isnull().sum()==0:
+        answered = answered.drop('answered', axis=1)
+        answered['answered'] = []
     random_number = random.randint(0,len(df))
+    while answered.loc[random_number,'answered'] == 'y':
+        random_number = random.randint(0,len(df))
+    answered.loc[random_number,'answered'] == 'y'
     print(f'Index is {random_number}')
     print(df['question'][random_number])
     print(df['a'][random_number])
     print(df['b'][random_number])
     print(df['c'][random_number])
-    print(df['d'][random_number])
-    print(df['e'][random_number])
-    print(df['f'][random_number])
-    print(df['g'][random_number])
-    print(df['h'][random_number])
-    print(df['i'][random_number])
-    print(df['j'][random_number])
-    print(df['k'][random_number])
-    print(df['l'][random_number])
-    print(df['m'][random_number])
-    x = input('What is your answer?: ')
-    print(df['answer'][random_number])
-    if np.isnan(df['chatgpt'][random_number]):
-        df['chatgpt'][random_number]=input('What does your research say?: ')
-    else:
-        print(df['chatgpt'][random_number])
-    if np.isnan(df['final_answer'][random_number]):
-        df['final_answer'][random_number]=input('What is the correct answer?: ')
-    else:
-        print(f"The correct answer is {df['final_answer'][random_number]}")
-    if x==df['final_answer'][random_number]:
+    if df['d'][random_number]!='nan':
+        print(df['d'][random_number])
+    if df['e'][random_number]!='nan':
+        print(df['e'][random_number])
+    if df['f'][random_number]!='nan':
+        print(df['f'][random_number])
+    if df['g'][random_number]!='nan':
+        print(df['g'][random_number])
+    if df['h'][random_number]!='nan':
+        print(df['h'][random_number])
+    if df['i'][random_number]!='nan':    
+        print(df['i'][random_number])
+    if df['j'][random_number]!='nan':
+        print(df['j'][random_number])
+    x = input('What is your answer?: ').lower()
+    print(f"The key is {df.loc[random_number,'key_letter']}")
+    print(df.loc[random_number, 'chat'])
+    answered.to_csv('answered.csv', index=False)
+    if x==df.loc[random_number,'key_letter']:
         return 'y'
-num_questions=int(input('How many questions do you want to answer?(intiger): '))
+num_question=1
 correct_answer=0
-for i in range(num_questions):
-    print(f'Question # {i+1} of {num_questions}')
+stop = 0
+while stop == 0:
+    print(f'Question # {num_question}')
     self_check=random_question(working_df)
     if self_check=='y':
         correct_answer+=1
-print(f'You got {round(correct_answer/num_questions*100,2)}% correct.')
+    s = input('Do you want to continue? (y/n): ').lower()
+    if s=='n':
+        stop=1
+    num_question+=1
+print(f'You got {round(correct_answer/(num_question-1)*100,2)}% correct.')
 working_df.to_csv('plab_chatgpt.csv', index=False)
